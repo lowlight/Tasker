cea36071015a919954dd65cfb4d0838e2497b983
Joshua Teague <joshua.j.teague.-ND@disney.com>
2014-05-09 12:09:33 -0700

    think i finally have it working.  needed to use the setLocal() to get the value back to tasked so it could be passed on to cooper.  in the switch statement i am still using case 216, without quotes around 216, i think that is ok but will see……  i left a couple of flashes in and simply commented out others, hopefully that will make mobile debugging a bit easier.

--- ClosestButToStop.js
+++ ClosestButToStop.js
@@ -3,33 +3,20 @@
  */
 
 if(rtcode != 200){
-	//bad get response
-	flash("bad GET response");
+	flash("GET error " + rtcode);
 	exit();
-} else {
-	//flash("rtcode good");
-}
+} 
 
 var json = eval ("(" + rtres + ")");
 var timerightnow = json.currentTime;
 timerightnow = timerightnow / 1000;
-//flash("time = " + timerightnow);
-var busarrivaltime = "0";
-
-var jsonTest = json.data.entry.arrivalsAndDepartures.length;
-flash("length " + jsonTest);
-
-//flash("route " + json.data.entry.arrivalsAndDepartures[0].routeShortName);
-
-//length: json.data.arrivalsAndDepartures.length
+//flash("length " + json.data.entry.arrivalsAndDepartures.length);
+busarrivaltime = "00";
 
 for(var i = 0; i < json.data.entry.arrivalsAndDepartures.length; i++) {
-	//flash("i " + i);
 	//flash("route " + json.data.entry.arrivalsAndDepartures[i].routeShortName);
-
 	switch(json.data.entry.arrivalsAndDepartures[i].routeShortName) {
 	case 216:
-		//flash("case 216");
 		if(json.data.entry.arrivalsAndDepartures[i].predictedArrivalTime == 0) {
 			busarrivaltime = json.data.entry.arrivalsAndDepartures[i].scheduledArrivalTime;
 		} else {
@@ -37,7 +24,6 @@ for(var i = 0; i < json.data.entry.arrivalsAndDepartures.length; i++) {
 		}
 		break;
 	case 218:
-		//flash("case 218");
 		if(json.data.entry.arrivalsAndDepartures[i].predictedArrivalTime == 0) {
 			busarrivaltime = json.data.entry.arrivalsAndDepartures[i].scheduledArrivalTime;
 		} else {
@@ -45,7 +31,6 @@ for(var i = 0; i < json.data.entry.arrivalsAndDepartures.length; i++) {
 		}
 		break;
 	case 219:
-		//flash("case 219");
 		if(json.data.entry.arrivalsAndDepartures[i].predictedArrivalTime == 0) {
 			busarrivaltime = json.data.entry.arrivalsAndDepartures[i].scheduledArrivalTime;
 		} else {
@@ -53,33 +38,20 @@ for(var i = 0; i < json.data.entry.arrivalsAndDepartures.length; i++) {
 		}
 		break;
 	default:
-		//flash("case default");
-		//flash("route = " + json.data.entry.arrivalsAndDepartures[i].routeShortName);
-		//for testing set busarrivaltime for final if
-		//busarrivaltime = 999999999999;
 		//do nothing
 	} //end switch
 	
-	
-	if(busarrivaltime != 0){
-		//flash("busarrivaltime != 0");
+	//flash("switch " + busarrivaltime);
+
+	if(busarrivaltime != "00"){
 		busarrivaltime = busarrivaltime / 1000;
-		//flash(busarrivaltime);
-		busarrivaltime = (busarrivaltime - timerightnow);
-		busarrivaltime = (busarrivaltime / 60);
-		//flash(busarrivaltime);
+		busarrivaltime = busarrivaltime - timerightnow;
+		busarrivaltime = busarrivaltime / 60;
 		busarrivaltime = busarrivaltime.toFixed(0);
-		//flash(busarrivaltime);
+		setLocal("%busarrivaltime", busarrivaltime);
+		//flash("set " + busarrivaltime);
 		exit();
 	} //end if
-	
 } //end for
-/*
-busarrivaltime = busarrivaltime / 1000;
-flash(busarrivaltime);
-busarrivaltime = (busarrivaltime - timerightnow);
-busarrivaltime = (busarrivaltime / 60);
-flash(busarrivaltime);
-busarrivaltime = busarrivaltime.toFixed(0);
-flash(busarrivaltime);
-*/
+//flash("end for loop");
+setLocal("%busarrivaltime", "--");
