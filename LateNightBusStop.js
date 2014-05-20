42080876cfc72efe15a0f7ad680866fc6915f5e1
Joshua Teague <joshua.j.teague.-ND@disney.com>
2014-05-20 08:45:40 -0700

    now with the rest call made in the javascript i needed different scripts for each stop.  plus i had to change the if statement for the late night one since it was on 554.

---
+++ LateNightBusStop.js
@@ -0,0 +1,58 @@
+/**
+ * FUNCTION DEFS
+ */
+
+ //function for making the rest call in javascript
+ function restCall() 
+{ 
+    var request = new XMLHttpRequest(); 
+    request.open("GET","http://api.pugetsound.onebusaway.org/api/where/arrivals-and-departures-for-stop/1_320.json?key=TEST&minutesAfter=0&minutesBefore=30",false);
+    request.send(); 
+    return(request.responseText); 
+} 
+
+
+//function to calc the minutes to bus arrival and display value
+function showMinTillArrival(timerightnow, busarrivaltime){
+	busarrivaltime = busarrivaltime / 1000;
+	busarrivaltime = timerightnow - busarrivaltime;
+	busarrivaltime = busarrivaltime / 60;
+	busarrivaltime = busarrivaltime.toFixed(0);
+	setLocal("%busarrivaltime", busarrivaltime);
+	flashLong("554 arrives in " + busarrivaltime + " minutes.");
+	exit();//i hope this kills the script......
+}
+
+/**
+* END FUNCTION DEFS
+*/
+
+flash("starting script");
+setLocal("%busarrivaltime", "!!");//something unique to show the script was run
+var rtres = restCall();
+//flashLong("rtres = " + rtres);
+var json = eval ("(" + rtres + ")");
+var timerightnow = 0;
+timerightnow = json.currentTime;
+if(timerightnow == 0){
+	flash("Rest returned no data");
+	setLocal("%busarrivaltime", "><");
+	exit();//no point in continuing, bailout now
+}
+
+timerightnow = timerightnow / 1000; //convert 2 seconds
+//flashLong("length " + json.data.entry.arrivalsAndDepartures.length);
+
+
+for(var i = 0; i < json.data.entry.arrivalsAndDepartures.length; i++) {
+	//flashLong(json.data.entry.arrivalsAndDepartures[i].routeShortName);
+	if(554 == json.data.entry.arrivalsAndDepartures[i].routeShortName){
+		if(json.data.entry.arrivalsAndDepartures[i].predictedArrivalTime == 0) {
+			showMinTillArrival(timerightnow, json.data.entry.arrivalsAndDepartures[i].scheduledArrivalTime);
+		} else {
+			showMinTillArrival(timerightnow, json.data.entry.arrivalsAndDepartures[i].predictedArrivalTime);
+		}
+	} 	
+} //end for
+
+setLocal("%busarrivaltime", "--");//no buses within timeframe set in rest call.
---
+++ MorningBusStop.js
@@ -0,0 +1,71 @@
+/**
+ * FUNCTION DEFS
+ */
+
+ //function for making the rest call in javascript
+ function restCall() 
+{ 
+    var request = new XMLHttpRequest(); 
+    request.open("GET","http://api.pugetsound.onebusaway.org/api/where/arrivals-and-departures-for-stop/1_64561.json?key=TEST&minutesAfter=0&minutesBefore=30",false);
+    request.send(); 
+    return(request.responseText); 
+} 
+
+
+//function to calc the minutes to bus arrival and display value
+function showMinTillArrival(timerightnow, busarrivaltime, busNum){
+	busarrivaltime = busarrivaltime / 1000;
+	busarrivaltime = timerightnow - busarrivaltime;
+	busarrivaltime = busarrivaltime / 60;
+	busarrivaltime = busarrivaltime.toFixed(0);
+	setLocal("%busarrivaltime", busarrivaltime);
+	flashLong(busNum + " arrives in " + busarrivaltime + " minutes");
+	exit();//i hope this kills the script......
+}
+
+/**
+* END FUNCTION DEFS
+*/
+
+flash("starting script");
+setLocal("%busarrivaltime", "!!");//something unique to show the script was run
+var busNum = 0;//var to pass the bus number to showMinTillArrival
+var rtres = restCall();
+//flashLong("rtres = " + rtres);
+var json = eval ("(" + rtres + ")");
+var timerightnow = 0;
+timerightnow = json.currentTime;
+if(timerightnow == 0){
+	flash("Rest returned no data");
+	setLocal("%busarrivaltime", "><");
+	exit();//no point in continuing, bailout now
+}
+
+timerightnow = timerightnow / 1000; //convert 2 seconds
+flash("length " + json.data.entry.arrivalsAndDepartures.length);
+
+
+for(var i = 0; i < json.data.entry.arrivalsAndDepartures.length; i++) {
+	//flashLong(json.data.entry.arrivalsAndDepartures[i].routeShortName);
+	if(216 == json.data.entry.arrivalsAndDepartures[i].routeShortName){
+		if(json.data.entry.arrivalsAndDepartures[i].predictedArrivalTime == 0) {
+			showMinTillArrival(timerightnow, json.data.entry.arrivalsAndDepartures[i].scheduledArrivalTime, 216);
+		} else {
+			showMinTillArrival(timerightnow, json.data.entry.arrivalsAndDepartures[i].predictedArrivalTime, 216);
+		}
+	} else if(218 == json.data.entry.arrivalsAndDepartures[i].routeShortName){
+		if(json.data.entry.arrivalsAndDepartures[i].predictedArrivalTime == 0) {
+			showMinTillArrival(timerightnow, json.data.entry.arrivalsAndDepartures[i].scheduledArrivalTime, 218);
+		} else {
+			showMinTillArrival(timerightnow, json.data.entry.arrivalsAndDepartures[i].predictedArrivalTime, 218);
+		}
+	} else if(219 == json.data.entry.arrivalsAndDepartures[i].routeShortName){
+		if(json.data.entry.arrivalsAndDepartures[i].predictedArrivalTime == 0) {
+			showMinTillArrival(timerightnow, json.data.entry.arrivalsAndDepartures[i].scheduledArrivalTime, 219);
+		} else {
+			showMinTillArrival(timerightnow, json.data.entry.arrivalsAndDepartures[i].predictedArrivalTime, 219);
+		}
+	}	
+} //end for
+
+setLocal("%busarrivaltime", "--");//no buses within timeframe set in rest call.
